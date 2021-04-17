fd561e4ec5519a8ae9b2371e58557403278b9504
Travis Taylor <travisdoesmath@gmail.com>
2021-04-17 00:07:21 -0500

    added pendulum code, fixed math error

--- example/pendulum.js
+++ example/pendulum.js
@@ -1,28 +1,3 @@
-function A(thetas) {
-    let M = [];
-    for (let i = 0; i < n; i++) {
-        let row = [];
-        for (let j = 0; j < n; j++) {
-            row.push((n - Math.max(i, j)) * Math.cos(thetas[i] - thetas[j]));
-        }
-        M.push(row)
-    }
-    return M;
-}
-
-function b(thetas, thetaDots) {
-    let v = [];
-    for (let i = 0; i < n; i++) {
-        let b_i = 0;
-        for (let j = 0; j < n; j++) {
-            b_i += (n - Math.max(i, j)) * Math.sin(thetas[i] - thetas[j]) * thetaDots[j] ** 2;
-        }
-        b_i += g * (n - i) * Math.sin(thetas[i]);
-        v.push(b_i);
-    }
-    return v;
-}
-
 class Pendulum {
     constructor(n = 4, thetas = Array(n).fill(0.5*Math.PI), thetaDots = Array(n).fill(0)) {
         this.n = n;
@@ -31,19 +6,57 @@ class Pendulum {
         this.g = -9.8;
     }
 
+    A(thetas) {
+        let M = [];
+        for (let i = 0; i < this.n; i++) {
+            let row = [];
+            for (let j = 0; j < this.n; j++) {
+                row.push((this.n - Math.max(i, j)) * Math.cos(thetas[i] - thetas[j]));
+            }
+            M.push(row)
+        }
+        return M;
+    }
+    
+    b(thetas, thetaDots) {
+        let v = [];
+        for (let i = 0; i < this.n; i++) {
+            let b_i = 0;
+            for (let j = 0; j < this.n; j++) {
+                b_i -= (this.n - Math.max(i, j)) * Math.sin(thetas[i] - thetas[j]) * thetaDots[j] ** 2;
+            }
+            b_i -= this.g * (this.n - i) * Math.sin(thetas[i]);
+            v.push(b_i);
+        }
+        return v;
+    }
+    
+    lagrange_rhs(thetas, thetaDots) {
+        let A = this.A(thetas);
+        let b = this.b(thetas, thetaDots);
+        return [thetaDots, math.lusolve(A, b).map(x => x[0])];
+    }
+    
+    RK4(dt, thetas, thetaDots) {
+        let k1 = this.lagrange_rhs(thetas, thetaDots);
+        let k2 = this.lagrange_rhs(thetas.map((x, i) => x + 0.5*dt*k1[0][i]), thetaDots.map((x, i) => x + 0.5*dt*k1[1][i]));
+        let k3 = this.lagrange_rhs(thetas.map((x, i) => x + 0.5*dt*k2[0][i]), thetaDots.map((x, i) => x + 0.5*dt*k2[1][i]));
+        let k4 = this.lagrange_rhs(thetas.map((x, i) => x +     dt*k3[0][i]), thetaDots.map((x, i) => x +     dt*k3[1][i]));
+  
+        let thetaDeltas    = math.add(k1[0], k2[0].map(x => 2 * x), k3[0].map(x => 2 * x), k4[0]).map(x => x * dt/6);
+        let thetaDotDeltas = math.add(k1[1], k2[1].map(x => 2 * x), k3[1].map(x => 2 * x), k4[1]).map(x => x * dt/6);
+    
+        return [math.add(thetas, thetaDeltas), thetaDots = math.add(thetaDots, thetaDotDeltas)]
+    }
+    
+
     tick(dt) {
-        let n = this.n;
-        let g = this.g;
         let thetas = this.thetas;
         let thetaDots = this.thetaDots;
 
-
-
-
-
-        let thetaDoubleDots = math.lusolve(A, b);
-        
-        return thetaDoubleDots;
+        let newState = this.RK4(dt, thetas, thetaDots);
+        this.thetas = newState[0];
+        this.thetaDots = newState[1];
     }
 
     get coordinates() {
